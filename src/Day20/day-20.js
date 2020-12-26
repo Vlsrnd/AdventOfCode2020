93dae1de228f00dc2127c67d737d7de305011188
Leonid Velichko <vlsrnd@gmail.com>
2020-12-26 22:50:33 +0300

    day 20 is complete

--- src/Day20/day-20.js
+++ src/Day20/day-20.js
@@ -2,57 +2,148 @@
 //input here data from day-20-data.txt
 const inputDataStr = ``;
 
+const reverse = (line) => line.split('').reverse().join('');
+const symbolToBinar = (line) => parseInt(line.replaceAll('.', '0').replaceAll('#', '1'), 2);
 
-class satelliteImage {
-  constructor(data) {
-    const arr = data.split('\n');
-    this.id = arr[0].replace(/\D/g, '');
-    const image = arr
-      .slice(1)
-      .map(line => line
-        .replace(/\./g, '0')
-        .replace(/#/g, '1'));
-    const [top, right, bottom, left] = [
-      image[0],
-      image.reduce((side, line) => side + line[line.length - 1], ''),
-      image[image.length - 1],
-      image.reduce((side, line) => side + line[0], '')
-    ];
-    this.rightDirection = [top, right, bottom.split('').reverse().join(''), left.split('').reverse().join('')]
-      .map(line => {
-        let test = parseInt(line, 2);
-        return test
-      });
-    this.leftDirection = [top.split('').reverse().join(''), left, bottom, right.split('').reverse().join('')]
-      .map(line => {
-        let test = parseInt(line, 2)
-        return test
-      });
-  }
-}
+const topSide = (tile) => tile[0];
+const bottomSide = (tile) => tile[tile.length - 1];
+const leftSide = (tile) => tile.map((x) => x[0]).join('');
+const rightSide = (tile) => tile.map((x) => x[x.length - 1]).join('');
 
-const data = inputDataStr.split('\n\n').map(image => new satelliteImage(image));
+const flip = (tile) => tile.map((line) => line.split('').reverse().join(''));
+const rotate = (tile) => tile
+  .map((row, i) => [...tile]
+    .reverse()
+    .map(line => line[i])
+    .join('')
+  );
 
-const partOne = (data) => {
-  let variants = new Map();
-  for (const image of data) {
-    for (const side of [...image.rightDirection, ...image.leftDirection]) {
-      if (!variants.has(side)) variants.set(side, 1);
-      else variants.set(side, variants.get(side) + 1);
-    }
-  }
-  const unique = [];
-  for (let value of variants) {
-    if (value[1] === 1) unique.push(value[0]);
-  }
-  let result = data.reduce((acc, img) => {
-    const validatedSide = img.rightDirection.filter(value => unique.includes(value)).length;
-    if (validatedSide === 2) acc *= img.id;
-    return acc;
-  }, 1);
-
-  return result;
+const getSides = (tile) => {
+  return [
+      topSide(tile),
+      bottomSide(tile),
+      leftSide(tile),
+      rightSide(tile),
+      reverse(bottomSide(tile)),
+      reverse(topSide(tile)),
+      reverse(rightSide(tile)),
+      reverse(leftSide(tile))
+    ]
+    .map(symbolToBinar);
 };
 
+const data = inputDataStr
+  .split('\n\n')
+  .map(tile => {
+    let [id, ...lines] = tile.split('\n');
+    id = Number(id.match(/\d+/g)[0])
+    return {
+      id,
+      tile: lines,
+      borders: getSides([...lines]),
+    }
+  });
 
-console.log("Part one answer:", partOne(data), "\nPart two answer:", '...in progress');
+const borderId = {};
+data.forEach(({id, borders}) => {
+  borders.forEach((x) => borderId[x] = borderId[x] ? [...borderId[x], id] : [id]);
+});
+
+const neighborsId = new Map();
+for (let id in borderId) {
+  const border = borderId[id];
+  if (border.length === 2) {
+    const neighbors = border.map((id) => neighborsId.get(id) || new Set());
+    neighborsId.set(border[0], neighbors[0].add(border[1]));
+    neighborsId.set(border[1], neighbors[1].add(border[0]));
+  }
+}
+const corners = Array.from(neighborsId.entries())
+    .filter(neighbors => neighbors[1].size === 2)
+    .map(n => n[0]);
+
+
+const partOne = (corners) => corners.reduce((a, b) => a * b, 1);
+
+const partTwo = (input, corners) => {
+  let image = [];
+  const corner = input.find((tile) => tile.id === corners[0]);
+  corner.tile = flip(corner.tile);
+
+  let [i, j] = [0, 0];
+  image[0] = [corner.tile];
+
+  while (true) {
+    while (true) {
+      const right = symbolToBinar(rightSide(image[j][i]));
+      const next = input.find(tile => tile.borders.includes(right));
+      if (!next) break;
+      input.splice(input.indexOf(next), 1)
+      if (next.borders.indexOf(right) > 3) {
+        next.borders = next.borders.slice(4);
+        next.tile = rotate(rotate(next.tile));
+      }
+      if (next.borders[0] === right) next.tile = flip(rotate(next.tile));
+      if (next.borders[1] === right) next.tile = rotate(next.tile);
+      if (next.borders[3] === right) next.tile = flip(next.tile);
+      image[j][i + 1] = next.tile;
+      i++;
+    }
+
+    i = 0;
+    const bottom = symbolToBinar(bottomSide(image[j][i]));
+    const next = input.find((x) => x.borders.includes(bottom));
+    if (!next) break;
+    input.splice(input.indexOf(next), 1)
+    if (next.borders.indexOf(bottom) > 3) {
+      next.borders = next.borders.slice(4);
+      next.tile = rotate(rotate(next.tile));
+    }
+    if (next.borders[1] === bottom) next.tile = flip(rotate(rotate(next.tile)));
+    if (next.borders[2] === bottom) next.tile = flip(rotate(next.tile));
+    if (next.borders[3] === bottom) next.tile = rotate(rotate(rotate(next.tile)));
+    image[j + 1] = [next.tile];
+    j++;
+  }
+
+  image = image.reduce((complete, row) => {
+    return [...complete, 
+      ...row.reduce((combine, tile) => {
+        tile = tile.slice(1, -1).map((line) => line.slice(1, -1));
+        return combine.map( (line, i) => line + tile[i] );
+      }, new Array(row[0].length - 2).fill(''))
+    ]}, []);
+  
+  const count = [
+    image,
+    rotate(image),
+    rotate(rotate(image)),
+    rotate(rotate(rotate(image))),
+    flip(image),
+    rotate(flip(image)),
+    rotate(rotate(flip(image))),
+    rotate(rotate(rotate(flip(image)))),
+  ].map((image) => {
+    const line1 = /^..................#./;
+    const line2 = /^#....##....##....###/;
+    const line3 = /^.#..#..#..#..#..#.../;
+    let count = 0;
+    for (let index = 0; index < image.length - 2; index++) {
+      for (let i = 0; i < image.length; i++) {
+        if (image[index].slice(i).match(line1) && 
+            image[index + 1].slice(i).match(line2) && 
+            image[index + 2].slice(i).match(line3)) {
+          count++;
+        }
+      }
+    }
+    return count;
+  });
+
+  const sum = image
+    .map((line) => line.split('').filter((char) => char === '#').length)
+    .reduce((a, b) => a + b);
+  return sum - count.find((x) => x !== 0) * 15;
+};
+
+console.log("Part one answer:", partOne(corners), "\nPart two answer:", partTwo(data, corners));
